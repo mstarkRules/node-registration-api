13643b0145038c0af43a2068bbe73a8028c7a45e
Marcos Amorim <markstm10@gmail.com>
2022-04-06 23:42:51 -0300

    feat: sets a error message to existing user exception

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,9 +4,18 @@ const User = require("../models/User");
 
 const router = express.Router();
 
+//create post route and save the data on db
 router.post("/register", async (req, res) => {
+  const { email } = req.body;
   try {
+    if (await User.findOne({ email })) {
+      return res.status(400).send({ error: "User already exists" });
+    }
+
     const user = await User.create(req.body);
+
+    //clears the passwords before return response
+    user.password = undefined;
 
     return res.send({ user });
   } catch (error) {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 require("dotenv/config");
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 require("./controllers/authController")(app);
 
