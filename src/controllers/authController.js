9b84fef2642a4ffedf222a8cd225368a1d79ba9a
Marcos Amorim <markstm10@gmail.com>
2022-04-06 21:14:05 -0300

    feat: configures router into auth controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,14 +4,14 @@ const User = require("../models/User");
 
 const router = express.Router();
 
-router.post("./register", async (req, res) => {
+router.post("/register", async (req, res) => {
   try {
     const user = await User.create(req.body);
 
-    return res.send({
-      user,
-    });
+    return res.send({ user });
   } catch (error) {
     return res.status(400).send({ error: "Registration failed" });
   }
 });
+
+module.exports = (app) => app.use("/auth", router);
