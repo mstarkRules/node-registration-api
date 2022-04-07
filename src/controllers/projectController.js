07a51c72a6dfc2738fde1ab90323fb0682af694f
Marcos Amorim <markstm10@gmail.com>
2022-04-07 17:51:21 -0300

    feat: creates a middleware to check if user is logged before giving him access

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const authMiddleware = require("../middlewares/auth");
+
+router.use(authMiddleware);
 router.get("/", (req, res) => {
   res.send({ ok: true });
 });
---
+++ src/middlewares/auth.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth.json");
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  //checks if there is an authorization token in the header
+  if (!authHeader) {
+    return res.status(401).send({ error: "No token provided" });
+  }
+
+  //splits the token parts to check each one
+  const parts = authHeader.split(" ");
+
+  //checks if there is 2 parts in the splited token
+  if (!parts.length === 2) {
+    return res.status(401).send({ error: "Token error" });
+  }
+
+  const [scheme, token] = parts;
+
+  //uses reges to checks if word 'Bearer' existis in header
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).send({ error: "Token malformatted" });
+  }
+
+  jwt.verify(token, authConfig.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: "Token invalid" });
+    }
+    req.userId = decoded.id;
+
+    return next();
+  });
+};
